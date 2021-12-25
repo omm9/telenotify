89ce49187f77637ed7efc74cf56ff840c311d2af
Om <email.swar@gmail.com>
2021-12-25 22:36:04 +0530

    update file
    added changes where it shows who is sending messages based on their id

--- firmware/telegram_msg_display.ino
+++ firmware/telegram_msg_display.ino
@@ -1,12 +1,21 @@
 #include "CTBot.h"
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+// Declaration for SSD1306 - SPI
+#define OLED_MOSI 23
+#define OLED_CLK 18
+#define OLED_DC 19
+#define OLED_CS 12
+#define OLED_RESET 13
 CTBot myBot;
 
 String ssid = "your_wifi_name_here";
 String pass = "your_wifi_pwd_here";
 String token = "1978155446:CCFb98rColFDcf92BKz9EeuVDRzzYcCxtGJ";
 
-void setup()
-{
+OledDisplay oledDisplay(int screen_width, int screen_height, int oled_mosi,int oled_clk,int oled_dc, int oled_reset, int oled_cs );
+void setup() {
   Serial.begin(9600);
 
   myBot.wifiConnect(ssid, pass);
@@ -14,7 +23,6 @@ void setup()
 
   delay(5000);
   pinMode(2, OUTPUT);
-  
   // Check if all things are ok
   if (myBot.testConnection()) {
     Serial.print("Telegram Bot Ready\n");
@@ -22,6 +30,10 @@ void setup()
   else {
     Serial.print("Connection problem\n");
   }
+  // put your setup code here, to run once:
+oledDisplay.init();
+oledDisplay.displayTextonOLED("Connecting to WIFI");
+
 }
 
 void loop()
@@ -30,21 +42,48 @@ void loop()
 
   // If there is an incoming message...
   if (myBot.getNewMessage(msg)) {
-    if (msg.text == "/start") {
-      Serial.print("Client ");
-      Serial.print(msg.sender.id);
-      Serial.write('\n');
-      //myBot.sendMessage(msg.sender.id, "Welcome to Idris Bot.");
-      digitalWrite(2,LOW);
-    }
-    else {
-      Serial.print(msg.text);
-      Serial.write('\n');
-      //myBot.sendMessage(msg.sender.id, "Text received.");
       digitalWrite(2,HIGH);
       delay(1000);
+    if (msg.text == "/off") {
+      if(msg.sender.id == 540135993){
+        Serial.print("Om   : ");
+      }else if(msg.sender.id == 1524460302){
+        Serial.print("Pedda: ");
+      }else if(msg.sender.id == 745440618){
+        Serial.print("Uday : ");
+      }else{
+        Serial.print("Client ");
+        Serial.print(msg.sender.id);
+        Serial.print(": ");
+      }
+
+      digitalWrite(2,LOW);
+      Serial.print("LED OFF ");
+      Serial.write('\n');
+      display.DisplayTextOnOled("LED OFF ")
     }
+    else{
+         
+            if(msg.sender.id == 540135993){
+              Serial.print("Om   : ");
+            }else if(msg.sender.id == 1524460302){
+              Serial.print("Pedda: ");
+              }else if(msg.sender.id == 745440618){
+              Serial.print("Uday : ");
+              }else{
+              Serial.print("Client ");
+              Serial.print(msg.sender.id);
+              Serial.print(": ");
+              }
+      Serial.print(msg.text); 
+      Serial.write('\n');
+      display.DisplayTextOnOled(msg.text)
+      //myBot.sendMessage(msg.sender.id, "Text received.");
+      //digitalWrite(2,HIGH);
+      //delay(1000);
+      }
   }
+  
 
   delay(500);
 }
